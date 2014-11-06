30a73654f265921d8ec351648ab900a865955aca
Brian Umbarger <brian.umbarger@gmail.com>
2014-11-05 19:09:49 -0500

    adding nme2 animations

---
+++ Assets/CrossDresser3/MixamoBasicLocomotionControlScript.cs
@@ -0,0 +1,122 @@
+using UnityEngine;
+using System.Collections;
+
+public class MixamoBasicLocomotionControlScript : MonoBehaviour, Mixamo.TransitionHandler {
+	
+	
+	public bool CanTransitionTo (string guard, string source, string destination)
+	{
+		if( guard == "block_on_player_control" ) {
+			return true;
+		} else {
+			return true;
+		}
+	}
+	
+	public string[] KeyControls() {
+		return keys;
+	}
+	
+	
+	AnimationStateMachine GetASM() {
+		return this.GetComponent<AnimationStateMachine>();
+	}
+	
+	// Use this for initialization
+
+
+	void Start () {
+		GetASM().SetTransitionHandler( this );
+		controller = GetComponent<CharacterController>();
+		asm = GetASM();
+	}
+	
+	public bool ShowGUIKey = Application.isEditor;
+	public float turnDegrees = 180f;
+	public float turnDegreesProne = 45f;
+	private float gravity = 20.0f;
+	private AnimationStateMachine asm;
+	private AnimationStateMachine.RootMotionResult result;
+	private CharacterController controller;
+	private Vector3 moveDirection = Vector3.zero;
+	private string[] keys ={
+		"W", "Forward",
+		"Space", "Jump",
+		"A", "Strafe Left",
+		"D", "Strafe Right",
+		"Q", "Turn Left",
+		"E", "Turn Right",
+	};
+	
+	void OnGUI() {
+		if( ShowGUIKey ) {
+			GUILayout.BeginVertical( GUILayout.ExpandWidth( true ) );
+			GUILayout.Label( "Key Options:" );
+			
+			// loop keys
+			
+			for (int i = 0; i < keys.Length; i += 2) {
+				GUILayout.Label(keys[i] + " - " + keys[i+1]);
+			}
+
+			GUILayout.EndVertical();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	AnimationStateMachine asm = GetASM();
+		int turnDirection = 0;
+			if( Input.GetKey( KeyCode.W ) ) {
+				asm.ChangeState( "move" );
+					
+			} else if( Input.GetKey( KeyCode.A ) ) {
+				asm.ChangeState( "walk_strafe_left" );
+			} 
+			
+			else if( Input.GetKey( KeyCode.D ) ) {
+				asm.ChangeState( "walk_strafe_right" );
+			} 
+			
+			else if( Input.GetKey( KeyCode.Q ) ) {
+				asm.ChangeState("turn_left");
+			} else if( Input.GetKey( KeyCode.E ) ) {
+				asm.ChangeState("turn_right");
+			}
+			
+			else {
+				asm.ChangeState( 0, "idle" );
+			}
+			
+			if( Input.GetKey( KeyCode.Space ) ) {
+				asm.ChangeState( "jump" );	
+			}
+			
+		
+		if( Input.GetKey( KeyCode.Q ) ) {
+			turnDirection = -1;
+		} else if( Input.GetKey( KeyCode.E ) ) {
+			turnDirection = 1;
+		}
+		
+		if( turnDirection != 0f ){
+			Vector3 forward = this.transform.forward;
+			forward.y = 0;
+			forward = forward.normalized;
+			Vector3 right = new Vector3(forward.z, 0, -forward.x);
+			transform.rotation = Quaternion.LookRotation( Vector3.RotateTowards( forward , right * turnDirection , turnDegrees * Mathf.Deg2Rad * Time.deltaTime , 1000f ) );
+		}
+	}
+	
+		void LateUpdate() {
+		if (controller != null){
+		//AnimationStateMachine asm = GetASM();
+			result = asm.GetRootMotion();
+			if( result != null ) {
+				// apply gravity
+				moveDirection.y -= gravity * Time.deltaTime;
+				controller.Move((moveDirection * Time.deltaTime) + result.GlobalTranslation);
+			}
+		}
+	}
+}
